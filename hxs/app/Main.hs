28ab711715b942e811466593c1fff9919362c972
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-11-19 01:26:12 +0000

    Configuration logic and CLI

--- hxs/app/Main.hs
+++ hxs/app/Main.hs
@@ -1,56 +1,121 @@
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE OverloadedRecordDot #-}
-{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE DuplicateRecordFields, OverloadedRecordDot #-}
+{-# LANGUAGE BlockArguments     #-}
 {-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveAnyClass     #-}
 {-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DerivingVia        #-}
+{-# LANGUAGE LambdaCase         #-}
 {-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE QuasiQuotes        #-}
+{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE ViewPatterns       #-}
+{-# OPTIONS_GHC -Wno-orphans    #-} -- JSON paths
 module Main where
-import Options.Generic
-import System.Directory.OsPath (getXdgDirectory, createDirectoryIfMissing, XdgDirectory(..))
-import qualified System.Directory.OsPath as OSDir
-import System.OsPath (osp, OsPath)
-import qualified System.OsPath as FP
+import Control.Exception
 import Data.Aeson
+import Options.Generic
+import System.Directory.OsPath
+import System.OsPath (osp, OsPath)
+import qualified Data.ByteString.Lazy as BSL
+import qualified System.OsPath as FP
+import Data.Maybe
+import Options.Applicative.Builder (InfoMod, progDesc, briefDesc)
+
+type XCodeGenPath = Maybe OsPath <?> "Path to the xcodegen executable"
+type CabalPath    = Maybe OsPath <?> "Path to the cabal executable"
 
 data Command w
-  = Configure
-    { xcodegen ::w::: OsPath <?> "Path to xcodegen executable"
-    , cabal    ::w::: OsPath <?> "Path to the cabal executable"
+  = Init
+    { xcodegen ::w::: XCodeGenPath
+    , cabal    ::w::: CabalPath
     }
-
-  | Init
-
   | Build
+    { xcodegen ::w::: XCodeGenPath
+    , cabal    ::w::: CabalPath
+    }
 
 data Configuration
   = Configuration
     { xcodegen :: OsPath
-    , cabal :: OsPath
+    , cabal    :: OsPath
     }
+
+infoMods :: InfoMod (Command Wrapped)
+infoMods = progDesc "hxs: Support tool for building projects using both Haskell and Swift"
 
 main :: IO ()
 main = do
-  command <- unwrapRecord "Haskell x Swift project tool"
+  command <- unwrap <$> getRecordWith infoMods mempty
+  user_config <- getConfig
   case command of
-    Configure{..}
-          -> configure Configuration{..}
-    Init  -> initialize
-    Build -> build
-  putStrLn "Hello, Haskell!"
-
-configure :: Configuration -> IO ()
-configure config = do
-  configPath <- getXdgDirectory XdgConfig [osp|hxs|]
-  createDirectoryIfMissing True configPath
+    Init{}  -> initialize
+    Build{} -> do
+      let config = applyToConfig user_config command
+      build config
 
 initialize :: IO ()
 initialize = putStrLn "Init is undefined"
 
-build :: IO ()
-build = putStrLn "Build is undefined"
+--------------------------------------------------------------------------------
+-- Build
+--------------------------------------------------------------------------------
+
+build :: Configuration -> IO ()
+build Configuration{..} = do
+
+--------------------------------------------------------------------------------
+-- Configuration
+--------------------------------------------------------------------------------
+
+getConfig :: IO Configuration
+getConfig = do
+  configDir <- getXdgDirectory XdgConfig [osp|hxs|]
+  createDirectoryIfMissing True configDir
+
+  let configPath = configDir FP.</> [osp|config.json|]
+  hasConfig <- doesPathExist configPath
+  legacyFP  <- FP.decodeUtf configPath
+
+  if hasConfig
+    then do
+      mconfig <- decode <$> BSL.readFile legacyFP
+      case mconfig of
+        Nothing     -> throwIO InvalidConfig
+        Just config -> return config
+    else do
+      config <- defaultConfig
+      BSL.writeFile legacyFP (encode config)
+      return config
+  where
+    defaultConfig :: IO Configuration
+    defaultConfig = do
+      xcodegen <- findExecutable [osp|xcodegen|]
+                    `orThrow` ProgNotFound "xcodegen" "brew install xcodegen"
+      cabal    <- findExecutable [osp|cabal|]
+                    `orThrow` ProgNotFound "cabal" "get cabal via GHCup"
+      return Configuration{..}
+
+orThrow :: Exception e => IO (Maybe a) -> e -> IO a
+orThrow act ex = act >>= \case Nothing -> throwIO ex; Just x -> pure x
+
+applyToConfig :: Configuration -> Command Unwrapped -> Configuration
+applyToConfig config cmd =
+  config
+    { xcodegen = fromMaybe config.xcodegen cmd.xcodegen
+    , cabal    = fromMaybe config.cabal    cmd.cabal
+    }
+
+--------------------------------------------------------------------------------
+-- Exceptions
+--------------------------------------------------------------------------------
+
+data HXSException
+  = ProgNotFound String String
+  | InvalidConfig
+
+deriving instance Show HXSException
+deriving instance Exception HXSException
 
 --------------------------------------------------------------------------------
 -- Instances
@@ -59,7 +124,23 @@ build = putStrLn "Build is undefined"
 -- Command
 deriving stock    instance Generic (Command w)
 deriving anyclass instance ParseRecord (Command Wrapped)
+
 -- Configuration
 deriving stock    instance Generic Configuration
 deriving anyclass instance ToJSON Configuration
 deriving anyclass instance FromJSON Configuration
+
+-- Path (utility to serialize OsPath)
+newtype Path = Path OsPath
+instance ToJSON Path where
+  toJSON (Path p) =
+    case FP.decodeUtf p of
+      Left e -> error (show e)
+      Right t -> toJSON t
+instance FromJSON Path where
+  parseJSON = withText "Path" $ \t -> do
+    case FP.encodeUtf (show t) of
+      Left e -> fail (show e)
+      Right p -> return $ Path p
+deriving via Path instance ToJSON OsPath
+deriving via Path instance FromJSON OsPath
