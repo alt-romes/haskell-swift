ddf635941fad1d2e51ab38615028129cdab02f42
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-11-24 15:55:47 +0000

    Add blank project for first hxs test

---
+++ hxs/test/AppT1/AppT1/AppT1App.swift
@@ -0,0 +1,17 @@
+//
+//  AppT1App.swift
+//  AppT1
+//
+//  Created by Rodrigo Mesquita on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppT1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hxs/test/AppT1/AppT1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppT1
+//
+//  Created by Rodrigo Mesquita on 24/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
