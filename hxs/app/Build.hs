6e48f1a7d843d79b4ea606c8a0afbac93c32717e
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-11-28 10:06:10 +0000

    hxs: Shake Oracle for flib path

--- hxs/app/Build.hs
+++ hxs/app/Build.hs
@@ -1,10 +1,16 @@
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BlockArguments, GeneralizedNewtypeDeriving, TypeFamilies #-}
 module Build where
 
+import GHC.Generics
+import Data.Typeable
+import Development.Shake.Classes
 import Development.Shake
 import Development.Shake.FilePath
 import Data.String.Interpolate
 import Common
+
+data CabalFLibPath = CabalFLibPath deriving (Show,Typeable,Generic,Eq,Hashable,Binary,NFData)
+type instance RuleResult CabalFLibPath = String
 
 --------------------------------------------------------------------------------
 -- Build
@@ -13,17 +19,22 @@ import Common
 build :: FilePath -> String -> IO ()
 build projDir projName = shake' projDir do
 
-  flib <- cabalForeignLibPath projDir projName
+  -- TODO: CreateDirectory if not exists for include and include-stubs
+
+  getFLibPath <- addOracle $ \CabalFLibPath -> do
+    cabalForeignLibPath projDir projName
 
   want [ projDir </> dynamicXCConfigFile
-       , flib
-       ]
+       , "foreign-shared-lib"
+       ] 
 
   projDir </> dynamicXCConfigFile %> \out -> do
+    flib <- getFLibPath CabalFLibPath
     -- ghc-pkg will return an absolute path, but this is not a problem for
     -- relocatable builds (i.e. not always running on the same directory, same
     -- machine) since this is done at build time into dirs that are not checked
     -- into source control, not at init time.
+    -- ROMES:TODO: ghc-pkg invocation should also be an oracle
     Stdout (words -> [_ffi_headers, rts_headers]) <- cmd "ghc-pkg field rts include-dirs --simple-output"
     writeFile' out [__i'E|
         HEADER_SEARCH_PATHS=$(inherit) #{rts_headers}
@@ -31,7 +42,8 @@ build projDir projName = shake' projDir do
     |]
 
   -- The shared library doubles as a stamp for everything else cabal builds e.g. the headers
-  flib %> \_out -> do
+  "foreign-shared-lib" ~> do
+    _flib <- getFLibPath CabalFLibPath
     cmd_ ([i|cabal build --project-dir=#{projDir} #{projName}-foreign|] :: String)
 
     -- Then copy all include-stubs//*_stubs.h to include//*.h
