f317307cc7793b1e9e0721fae73da0d66b1ea5d9
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-12-09 01:18:12 +0000

    Improve Foreign Function Wrappers
    * Re-work the generation code
    * Treat StablePtrs differently, without serialization
    * Add tests

--- swift-ffi/src/Foreign/Swift.hs
+++ swift-ffi/src/Foreign/Swift.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE DerivingStrategies #-}
 module Foreign.Swift
-  ( foreignExportSwift )
+  ( foreignExportSwift
+    -- ** Re-exports
+  , BS.toStrict
+  , Generic
+  , module Data.Aeson
+  )
   where
 
+import GHC.Generics
+import qualified Data.ByteString.Lazy as BS
+import Control.Monad.State
 import Data.Aeson
 import Foreign.C
+import Foreign.StablePtr
 import Foreign.Marshal
 import Foreign.Ptr
 import Language.Haskell.TH
@@ -38,65 +48,183 @@ import Foreign.Storable
 -- (de)serialization and calling the Haskell wrapper function.
 foreignExportSwift :: Name -> Q [Dec]
 foreignExportSwift fun_name = do
-  let wrapper_name_str = ('h':) (nameBase fun_name)
+  let wrapper_name_str = 'h':nameBase fun_name
       wrapper_name = mkName wrapper_name_str
   callresult_name <- newName "callresult"
   fun_ty     <- reifyType fun_name
   wrapper_ty <- makeWrapperTy fun_ty
-  unless ((tyFunArity fun_ty)*2 + 2 == tyFunArity wrapper_ty)
-    $ error $ unlines [ "wrapper_ty arity does not match fun_ty arity:"
-                      , "wrapper_ty: " ++ show wrapper_ty
-                      , "fun_ty: " ++ show fun_ty
-                      ]
+
   let
     fexp = ForeignD $ ExportF CCall wrapper_name_str wrapper_name wrapper_ty
     fsig = SigD wrapper_name wrapper_ty
 
   -- Vars of original function
-  orgVars <- mapM (const $ newName "org")
+  orgVars <- mapM (const $ newName "orig")
                   [1..tyFunArity fun_ty]
-  -- Actual vars
-  argVars <- mapM (\x -> newName $ (if odd x then "cstr" else "clen"))
-                  [1..tyFunArity fun_ty * 2]
-  -- Two arguments to write the output to
-  let buffer = mkName "buffer"
-      sizeptr = mkName "size_ptr"
-  fun <- FunD wrapper_name . (:[]) . (\b -> Clause (map VarP (argVars ++ [buffer, sizeptr])) (NormalB b) []) . DoE Nothing <$> do
-           binds <-
-             forM (zip3 orgVars argVars (drop 1 argVars)) \(org, varE -> cstr, varE -> clen) -> do
-               BindS (VarP org) <$> [| unsafePackCStringLen ($cstr, $clen) >>= throwDecodeStrict |]
-           let applyF = foldl AppE (VarE fun_name) (map VarE orgVars)
-               resultBind
-                | resultIsIO fun_ty = BindS (VarP callresult_name) applyF
-                | otherwise = LetS [ValD (VarP callresult_name) (NormalB applyF) []]
-           body <-
-             [| unsafeUseAsCStringLen (toStrict $ encode $(varE callresult_name)) $ \(ptr,len) -> do
-                   size_avail <- peek $(varE sizeptr)
-                   -- Write actual size to intptr
-                   -- We always do this, either to see if we've overshot the buffer, or to
-                   -- know the size of what has been written.
-                   poke $(varE sizeptr) len
-                   if size_avail < len
-                      then do
-                        -- We need @len@ bytes available
-                        -- The caller has to retry
-                        return ()
-                      else do
-                        moveBytes $(varE buffer) ptr len
-              |]
-           return (binds++[resultBind]++[NoBindS body])
+
+  -- Vars of the wrapper function
+  -- We make no assumptions about how results are returned (serialized vs
+  -- directly), we simply allocate names for every argument to the wrapper
+  -- function, and take them on demand when constructing the function body.
+  argVars <- mapM (const $ newName "wrap")
+                  [1..tyFunArity wrapper_ty]
+
+  fun <- FunD wrapper_name . (:[]) . (\b -> Clause (map VarP argVars) (NormalB b) []) . DoE Nothing <$> do
+    buildBodyWithArgs argVars $ do
+      binds <- zipWithM decodeArg orgVars (tyFunArgsTy fun_ty)
+      let applyF = foldl AppE (VarE fun_name) (map VarE orgVars)
+          resultBind
+           | resultIsIO fun_ty = BindS (VarP callresult_name) applyF
+           | otherwise = LetS [ValD (VarP callresult_name) (NormalB applyF) []]
+      final <- encodeRes (tyFunResTy fun_ty) callresult_name
+      return (binds++[resultBind]++[NoBindS final])
   return [fsig, fun, fexp]
 
+--------------------------------------------------------------------------------
+-- Foreign-wrapper builder monad
+--------------------------------------------------------------------------------
 
--- | ROMES:TODO: Very incomplete (e.g. ForallT)
+-- | A monad that helps build the body of the foreign function wrapper that is
+-- exposed.
+--
+-- In essence, the number of arguments the wrapper receives for each argument
+-- of the original function and return result depend on the types of these
+-- arguments, so, when constructing a result or reading an argument, we must
+-- lazily take in order the amount of variables we expect to be available to
+-- the function to convert that single argument from the list of function
+-- arguments.
+--
+-- By the time we handled all arguments and result the list of allocated names
+-- for the foreign function wrapper should be empty.
+newtype BodyBuilder a = BodyBuilder (StateT [Name] Q a)
+  deriving newtype (Functor, Applicative, Monad, MonadFail)
+
+instance Quote BodyBuilder where
+  newName = BodyBuilder . lift . newName
+
+-- | Run a body building action with the given allocated foreign wrapper
+-- functions.  By the time we are done body building, we should have used
+-- exactly all allocated names.
+buildBodyWithArgs :: [Name] -> BodyBuilder a -> Q a
+buildBodyWithArgs names (BodyBuilder bb) = do
+  (a, []) <- runStateT bb names
+  return a
+
+-- | Take @N@ arguments from the arguments allocated for the foreign wrapper function
+-- ROMES:TODO: Vecs and Nats!!
+needArgs :: Int -> BodyBuilder [Name]
+needArgs n = BodyBuilder $ do
+  needed <- gets (take n)
+  modify (drop n)
+  return needed
+
+--------------------------------------------------------------------------------
+-- Encoding and Decoding arguments
+--------------------------------------------------------------------------------
+
+-- | Decodes an argument from the wrapper functions and binds it to the name of the original argument (to eventually pass to the function call that we are wrapping)
+decodeArg :: Name
+          -- ^ The name to which the decoded argument should be bound
+          -> Type
+          -- ^ The type of the decoded argument
+          -> BodyBuilder Stmt
+decodeArg orig (AppT (ConT n) _a)
+  | n == ''StablePtr
+  = do
+    -- Stable pointers are kept unchanged
+    [stable_ptr] <- needArgs 1
+    return $ LetS [ValD (VarP orig) (NormalB (VarE stable_ptr)) []]
+decodeArg orig _ty
+  = do
+    -- Otherwise we decode using JSON
+    [cstr, clen] <- needArgs 2
+    BindS (VarP orig) <$> [| unsafePackCStringLen ($(varE cstr), $(varE clen)) >>= throwDecodeStrict |]
+
+
+-- | Encodes the result from the wrapped function into the wrapper's return format.
+-- For most data types, the result will be serialized into the final two arguments.
+-- However, some, like 'StablePtr', will be simply returned.
+encodeRes :: Type
+          -- ^ The original function's return type
+          -> Name
+          -- ^ The name to which the result of evaluating the wrapped function is bound
+          -> BodyBuilder Exp
+encodeRes (stripTyIO -> AppT (ConT n) _b) callresult_name
+  -- StablePtrs are simply returned
+  | n == ''StablePtr
+  = [| return $(varE callresult_name) |]
+encodeRes _ callresult_name
+  -- In all other cases try to serialize the result
+  = do
+  [buffer, sizeptr] <- needArgs 2
+  [| unsafeUseAsCStringLen (toStrict $ encode $(varE callresult_name)) $ \(ptr,len) -> do
+        size_avail <- peek $(varE sizeptr)
+        -- Write actual size to intptr
+        -- We always do this, either to see if we've overshot the buffer, or to
+        -- know the size of what has been written.
+        poke $(varE sizeptr) len
+        if size_avail < len
+           then do
+             -- We need @len@ bytes available
+             -- The caller has to retry
+             return ()
+           else do
+             moveBytes $(varE buffer) ptr len
+   |]
+
+--------------------------------------------------------------------------------
+-- Foreign exported types
+--------------------------------------------------------------------------------
+
+-- | Create the type of the function that will be exported from the type of the
+-- original function which will be wrapped.
+--
+-- ROMES:TODO: Still incomplete (e.g. ForallT)
 makeWrapperTy :: Type -> Q Type
-makeWrapperTy (AppT (AppT ArrowT _a) b) = [t| (Ptr CChar) -> Int -> $(makeWrapperTy b) |]
-makeWrapperTy _x = [t| Ptr CChar -> Ptr Int -> IO () |]
+-- Wrapping an argument
+makeWrapperTy (AppT (AppT ArrowT (AppT (ConT n) a)) b)
+  -- For StablePtrs keep the arg unchanged
+  | n == ''StablePtr
+  = [t| StablePtr $(pure a) -> $(makeWrapperTy b) |]
+makeWrapperTy (AppT (AppT ArrowT _a) b)
+  -- For anything else serialize using a char* and size_t
+  = [t| Ptr CChar -> Int -> $(makeWrapperTy b) |]
 
--- | ROMES:TODO: Very incomplete (e.g. ForallT)
+-- Wrapping the result
+makeWrapperTy (AppT (ConT io) (AppT (ConT stable_ptr) a))
+  -- StablePtrs can only be returned in IO actions, so we only check if result
+  -- is IO (StablePtr _) and not (StablePtr _)
+  | io == ''IO
+  , stable_ptr == ''StablePtr
+  = [t| IO (StablePtr $(pure a)) |]
+makeWrapperTy _x
+  -- Arguments for the result if it cannot be trivially converted
+  = [t| Ptr CChar -> Ptr Int -> IO () |]
+
+--------------------------------------------------------------------------------
+-- Utils
+--------------------------------------------------------------------------------
+
+-- | ROMES:TODO: Still incomplete (e.g. ForallT)
 tyFunArity :: Type -> Int
 tyFunArity (AppT (AppT ArrowT _) b) = 1 + tyFunArity b
 tyFunArity _ = 0
+
+-- | Extract the types of each argument of the given function type into a list
+tyFunArgsTy :: Type -> [Type]
+tyFunArgsTy (AppT (AppT ArrowT a) b) = a : tyFunArgsTy b
+tyFunArgsTy _ = []
+
+-- | Extract the result type of the given function type
+tyFunResTy :: Type -> Type
+tyFunResTy (AppT (AppT ArrowT _) b) = tyFunResTy b
+tyFunResTy x = x
+
+-- | Remove an IO tycon from the given type. This is used to normalize the
+-- return type before determining how to return it in the wrapper.
+stripTyIO :: Type -> Type
+stripTyIO (AppT (ConT n) t) | n == ''IO = t
+stripTyIO t = t
 
 resultIsIO :: Type -> Bool
 resultIsIO (AppT (AppT ArrowT _) b) = resultIsIO b
--- swift-ffi/test/Main.hs
+++ swift-ffi/test/Main.hs
@@ -1,4 +1,48 @@
+{-# LANGUAGE DerivingStrategies, DeriveAnyClass #-}
 module Main (main) where
+
+import GHC.Generics
+import Foreign.StablePtr
+import Foreign.Swift
+
+data User = User
+  { name :: String
+  , age  :: Int
+  }
+  deriving stock Generic
+  deriving anyclass (FromJSON, ToJSON)
+
+birthday :: User -> User
+birthday u = u{age = age u + 1}
+$(foreignExportSwift 'birthday)
+
+f1 :: User -> IO User
+f1 = undefined
+$(foreignExportSwift 'f1)
+
+f2 :: User -> IO (StablePtr User)
+f2 = undefined
+$(foreignExportSwift 'f2)
+
+f3 :: StablePtr User -> User
+f3 = undefined
+$(foreignExportSwift 'f3)
+
+f4 :: StablePtr User -> IO User
+f4 = undefined
+$(foreignExportSwift 'f4)
+
+f5 :: StablePtr User -> IO (StablePtr User)
+f5 = undefined
+$(foreignExportSwift 'f5)
+
+f6 :: User -> StablePtr User -> User -> IO (StablePtr User)
+f6 = undefined
+$(foreignExportSwift 'f6)
+
+f7 :: User -> StablePtr User -> User -> IO User
+f7 = undefined
+$(foreignExportSwift 'f7)
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
